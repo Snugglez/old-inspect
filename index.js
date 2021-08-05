f08a0b38adfb5863d2e97f0ee6f501c5756243ec
Snugglez <32309975+Snugglez@users.noreply.github.com>
2021-08-05 13:21:50 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = function testmod(d) {d.hook('C_REQUEST_USER_PAPERDOLL_INFO', 3, (e) => {e.zoom = true; return true})}
+module.exports = function testmod(d) {
+  d.dispatch.addDefinition('C_REQUEST_USER_PAPERDOLL_INFO', 4, [
+        ['name', 'refString'],
+        ['serverId', 'uint32'],
+        ['zoom', 'bool'],
+        ['name', 'string']
+      ], false);
+  d.hook('C_REQUEST_USER_PAPERDOLL_INFO', 4, (e) => {e.zoom = true; return true})
+
+}
