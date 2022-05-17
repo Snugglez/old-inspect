3007c3862e088dbbf81d603576615dd62c6739c2
Snugglez <32309975+Snugglez@users.noreply.github.com>
2022-05-16 23:11:09 -0400

    v92 support
    removed hardcoded def since its been added to tera-data directly, changed version to '*' since v92 forks use the proper latest def for their patch version

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 module.exports = function testmod(d) {
-  d.dispatch.addDefinition('C_REQUEST_USER_PAPERDOLL_INFO', 4, [
-        ['name', 'refString'],
-        ['serverId', 'uint32'],
-        ['zoom', 'bool'],
-        ['name', 'string']
-      ], false);
-  d.hook('C_REQUEST_USER_PAPERDOLL_INFO', 4, (e) => {e.zoom = true; return true})
-
+  d.hook('C_REQUEST_USER_PAPERDOLL_INFO', '*', (e) => { e.zoom = true; return true })
 }
